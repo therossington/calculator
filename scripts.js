833cee82f4933d1c2ff6f5e36e00efad9624e339
therossington <camsymonds@outlook.com>
2021-09-08 13:47:18 +1000

    Work on JS

--- scripts.js
+++ scripts.js
@@ -1,32 +1,43 @@
-const button = document.querySelectorAll('button');
+const operatorInput = document.querySelectorAll('.operators');
+const numberInput = document.querySelectorAll('.numbers');
+const equals = document.getElementById('equals');
+const clear = document.getElementById('clear');
 const display = document.getElementById('content');
 let inputArray = [];
-
-//Evaluates each click for entry.
-
-let numOperations = '';
-
-button.forEach(button => button.addEventListener('click', () => {
-    if (button.id == 'clear') {
-            display.innerHTML = '';
-            display.textContent = '0';
-            inputArray = [];
-     } else if (button.id != 'clear') {
-            inputArray.push(button.textContent);
-            display.textContent = inputArray.join('');
-            return console.log(inputArray.join(''));
-     }
-    }
-)       
-);
-
 let operator = '';
-let firstInput = inputArray;
-let secondInput = inputArray;
+let firstInput = '';
+let secondInput = '';
 
+//Collects number input.
+
+numberInput.forEach(numberInput => numberInput.addEventListener('click', () => {
+    inputArray.push(numberInput.textContent);
+    display.textContent = inputArray.join('');
+    if (operator == '') {
+        firstInput = inputArray.join('');
+    } else if (operator != '') {
+        secondInput = inputArray.join('');
+    }
+}));
+
+//Triggers clear actions.
+
+clear.addEventListener('click', () => {
+    display.innerHTML = '';
+    display.textContent = '0';
+    inputArray = [];
+    operator = '';
+})
+
+//Selects operator.
+
+operatorInput.forEach(operatorInput => operatorInput.addEventListener('click', (e) => {
+    operator = e.target.id; 
+    inputArray = [];
+}));
 
 function add(firstInput,secondInput) {
-    return firstInput + secondInput;
+    return parseInt(firstInput) + parseInt(secondInput);
 }
 
 function subtract(firstInput, secondInput) {
@@ -42,13 +53,13 @@ function divide(firstInput, secondInput) {
 }
 
 function operate() {
-    if (operator === '+') {
+    if (operator === 'multiply') {
         return add(firstInput, secondInput);
-    } else if (operator === '-') {
+    } else if (operator === 'subtract') {
         return subtract(firstInput, secondInput);
-} else if (operator === '&times') {
+} else if (operator === 'multiply') {
         return multiply(firstInput, secondInput);
-} else if (operater === '&divide') {
+} else if (operator === 'divide') {
         return divide(firstInput, secondInput);
 } else return 'ERROR!';
 }
