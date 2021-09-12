2245577709fc99515f95bdff2f20fb666512123a
therossington <camsymonds@outlook.com>
2021-09-12 16:41:26 +1000

    Refactor JS again

--- scripts.js
+++ scripts.js
@@ -1,47 +1,58 @@
-const calculator = document.querySelector('.calculator');
-const keypad = document.querySelector('.keypad');
-const display = document.getElementById('content');
+const numberSelect = document.querySelectorAll('.numbers');
+const operatorSelect = document.querySelectorAll('.operators');
+const equalsSelect = document.getElementById('equals');
+const clearSelect = document.getElementById('clear');
+const previousOperandDisplay = document.getElementById('previous-operand');
+const currentOperandDisplay = document.getElementById('current-operand');
 
-//Checks what user inputs - for testing.
-
-keypad.addEventListener('click', (e) => {
-    if (e.target.matches('button')) {
-        const key = e.target;
-        const action = key.dataset.action;
-        if (!action) {
-            console.log('number key');
-        } else if (
-            action === 'addition' ||
-            action === 'subtract' ||
-            action === 'multiply' ||
-            action === 'divide'
-        ) {
-            console.log('operator key');
-        } else if (action === 'decimal') {
-            console.log('decimal key');
-        } else if (action === 'clear') {
-            console.log('clear key');
-        } else if (action === 'equals') {
-            console.log('equals key');
-        };
+class Calculator {
+    constructor(previousOperandDisplay, currentOperantDisplay) {
+        this.previousOperandDisplay = previousOperandDisplay;
+        this.currentOperantDisplay = currentOperantDisplay;
+        this.clear();
     };
+};
+
+const calculator = new Calculator(previousOperandDisplay, currentOperandDisplay);
+
+function clear() {
+    this.currentOperand = '';
+    this.previousOperand = '';
+    this.operation = '';
+}
+
+numberSelect.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.textContent);
+        calculator.updateDisplay();
+    })
 });
 
-//Number display - how append non-zero and decimals to the display.
-
-keypad.addEventListener('click', (e) => {
-    if (e.target.matches('button')) {
-        const key = e.target;
-        const action = key.dataset.action;
-        const content = key.textContent;
-        const number = display.textContent;
-        if (!action) {
-            if (number === '0') {
-                display.textContent = content;
-            } else {
-                display.textContent = number + content;
-        }} else if (action === 'decimal') {
-            display.textContent = number + '.';
-        };
-    };
+operatorSelect.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.textContent);
+        calculator.updateDisplay();
+    });
 });
+
+function backspace() {
+
+};
+
+function appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) {
+        return this.currentOperand = this.currentOperand.toString() + number.toString();
+    };
+};
+
+function chooseOperation(a) {
+
+};
+
+function operate() {
+
+};
+
+function updateDisplay() {
+    this.currentOperandDisplay.textContent = this.currentOperand;
+};
