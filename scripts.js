cd5013ef05cb31b547b278a2ec859c3bfd90b01c
therossington <camsymonds@outlook.com>
2021-09-18 10:28:41 +1000

    Refactor js

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ let inputArray = [];
 let operator = '';
 let firstInput = '';
 let secondInput = '';
+let previousResult = '';
 let result = '';
 
 //Collects number input.
@@ -20,10 +21,9 @@ numberInput.forEach(numberInput => numberInput.addEventListener('click', () => {
         console.log("First input = "+firstInput);
     } else if (operator != '' && result == '') {
         secondInput = inputArray.join('');
-        // operate(firstInput, secondInput);       
         console.log("Second input = "+secondInput);
     } else if (operator != '' && result != '') {
-        firstInput = result;
+        firstInput = previousResult;
         secondInput = inputArray.join('');
         console.log("First input = "+firstInput);
         console.log("Second input = "+secondInput);
@@ -31,7 +31,9 @@ numberInput.forEach(numberInput => numberInput.addEventListener('click', () => {
 }));
 
 equals.addEventListener('click', (e) => {
+    operate();
     display.textContent = result;
+    inputArray = [];
     return console.log(result + e.target.id);
 });
 
@@ -54,45 +56,61 @@ operatorInput.forEach(operatorInput => operatorInput.addEventListener('click', (
     inputArray = [];
     console.log(operator);
     if (secondInput != '') {
-        operate(firstInput, secondInput);
+        operate();
         console.log("Result = " + result);
         display.textContent = result;
     }
 }));
 
-function add(a, b) {
+/* function add(a, b) {
     result = parseFloat(a) + parseFloat(b);
     return result;
 }
 
 function subtract(a, b) {
     result = parseFloat(a) - parseFloat(b);
-    return result;
 }
 
 function multiply(a, b) {
     result = parseFloat(a) * parseFloat(b);
-    return result;
 }
 
 function divide(a, b) {
     result =  parseFloat(a) / parseFloat(b);
-    return result;
-}
+} */
 
 function operate() {
-    if (operator === 'addition') {
-        return add(firstInput, secondInput);
+    switch(operator) {
+        case 'addition':
+            result = parseFloat(firstInput) + parseFloat(secondInput);
+            //previousResult = result;
+            break;
+        case 'subtract':
+            result = parseFloat(firstInput) - parseFloat(secondInput);
+            //previousResult = result;
+            break;
+        case 'multiply':
+            result = parseFloat(firstInput) * parseFloat(secondInput);
+            //previousResult = result;
+            break;
+        case 'divide':
+            result = parseFloat(firstInput) / parseFloat(secondInput);
+            //previousResult = result;
+            break;
+    };
+};
+           
+    /* if (operator === 'addition') {
+        result = parseFloat(firstInput) + parseFloat(secondInput);
+        return result;
     } else if (operator === 'subtract') {
-        return subtract(firstInput, secondInput);
-} else if (operator === 'multiply') {
-        return multiply(firstInput, secondInput);
-} else if (operator === 'divide') {
-        return divide(firstInput, secondInput);
-} else return 'ERROR!';
-}
-
-equals.addEventListener('click', (e) => {
-    return console.log(result + e.target.id);
-});
-
+        result = parseFloat(firstInput) - parseFloat(secondInput);
+        return result;
+    } else if (operator === 'multiply') {
+        result = parseFloat(firstInput) * parseFloat(secondInput);
+        return result;
+    } else if (operator === 'divide') {
+         result = parseFloat(firstInput) / parseFloat(secondInput);
+         return result;
+    } else return 'ERROR!';
+    } */
